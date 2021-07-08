56fe3f920774e07a64b2011e2eec89f39228d5db
Ameer Mousavi <xerxes1718@gmail.com>
2021-07-08 23:08:50 +0430

    init

---
+++ precache-manifest.989b6b2445d5f1c72ce8f8988b776535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619e97d283e88e17f0b3",
+    "url": "/css/app.34d36af3.css"
+  },
+  {
+    "revision": "5d9bf64ae276bb9f81ab5ee20dd493b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "619e97d283e88e17f0b3",
+    "url": "/js/app.5ccae63f.js"
+  },
+  {
+    "revision": "8533b7afaab863bc0411",
+    "url": "/js/chunk-vendors.b8c0eb0e.js"
+  },
+  {
+    "revision": "6bd2a6eded0c8e1720e1ca60d3b0d870",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
